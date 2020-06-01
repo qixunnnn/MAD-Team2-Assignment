d0f1783111f4181bd5167ab1cb21c93af8933c5a
Qi Xun <qixunnn@gmail.com>
2020-06-01 23:14:47 +0800

    Started school activity

---
+++ app/src/main/java/sg/edu/sg/mad_t02_assignment/schoolactivity.java
@@ -0,0 +1,17 @@
+package sg.edu.sg.mad_t02_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class schoolactivity extends AppCompatActivity {
+
+    String s1, s2;
+    int images [] = {};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schoolactivity);
+    }
+}
