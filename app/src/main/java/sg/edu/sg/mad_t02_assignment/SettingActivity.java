afb823f7ee7b15861449e15148fbb6fcd565b603
jw <wyipjun@gmail.com>
2020-06-04 05:16:31 +0800

    added change password page - Yip Jun Wei

--- app/src/main/java/sg/edu/sg/mad_t02_assignment/SettingActivity.java
+++ app/src/main/java/sg/edu/sg/mad_t02_assignment/SettingActivity.java
@@ -18,6 +18,7 @@ import java.util.Set;
 public class SettingActivity extends AppCompatActivity {
 
     private Button signOutButton;
+    private Button ChangePassButton;
     private SharedPreferences settingsPreferences;
     private SharedPreferences.Editor settingsPrefsEditor;
     AlertDialog.Builder builder;
@@ -28,16 +29,28 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         signOutButton = findViewById(R.id.signOutBtn);
+        ChangePassButton = findViewById(R.id.ChangePassBtn);
         settingsPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
         settingsPrefsEditor = settingsPreferences.edit();
         builder = new AlertDialog.Builder(this);
+        signOutButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent = new Intent(SettingActivity.this,ChangePassActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                return false;
+            }
+        });
 
         signOutButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
-                builder.setMessage("By clicking Yes, you will be brought to the login page and Stay logged in checkbox for you")
+                //alert for user to confirm sign out
+                builder.setMessage("By clicking Yes, you will be brought to the login page and Stay logged in checkbox will be unticked for you")
                         .setCancelable(false)
+                        //when user press yes
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
@@ -48,6 +61,7 @@ public class SettingActivity extends AppCompatActivity {
                                 Log.v(TAG, "User accept!");
                             }
                         })
+                        //when user press no
                         .setNegativeButton("No", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 //  Action for 'NO' Button
