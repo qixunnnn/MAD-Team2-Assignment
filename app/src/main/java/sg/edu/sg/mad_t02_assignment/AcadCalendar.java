c4c19bfd81577e37119e0316e9661137b2b89db7
Guan Teck Tan <s10198165@connect.np.edu.sg>
2020-06-05 00:03:30 +0800

    Countdown day add - guanteck

--- app/src/main/java/sg/edu/sg/mad_t02_assignment/AcadCalendar.java
+++ app/src/main/java/sg/edu/sg/mad_t02_assignment/AcadCalendar.java
@@ -1,13 +1,42 @@
 package sg.edu.sg.mad_t02_assignment;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class AcadCalendar extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acadcalendar);
+        TextView countdownDate = findViewById(R.id.coutdown);
+        try{
+            Date cDate = new Date();
+            String endDate = "06/15/2020";
+            Date eDate;
+            SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy");
+            eDate = date.parse(endDate);
+            long diff = eDate.getTime() - cDate.getTime();
+            long diffInDate = diff / (24 * 60 * 60 * 1000);
+            String dayDifference = Long.toString(diffInDate);
+            countdownDate.setText(dayDifference + " Days left to holidays!");
+        } catch (Exception e){
+
+        }
+
+
+
+
+
+
+
+
     }
 }
