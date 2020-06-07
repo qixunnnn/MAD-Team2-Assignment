d76a3d4c17fa53b5df8c7f9a8205815a53eb1fb0
Qi Xun <qixunnn@gmail.com>
2020-06-07 23:05:36 +0800

    ResultActivity added

--- app/src/main/java/sg/edu/sg/mad_t02_assignment/SignUpActivity.java
+++ app/src/main/java/sg/edu/sg/mad_t02_assignment/SignUpActivity.java
@@ -50,8 +50,6 @@ public class SignUpActivity extends AppCompatActivity {
                 et.setTransformationMethod(new PasswordTransformationMethod());
             }
 
-
-
             return true;
         }
     };
